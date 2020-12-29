786bb49f2f6a0c385a205a90b5975ed6b9378694
Yna Ramos <yna.francesca.ramos@gmail.com>
2020-12-29 16:47:04 +0000

    database populated :-) hope it works

--- DataAccess/DataModels/ProductAvailability.cs
+++ DataAccess/DataModels/ProductAvailability.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DataAccess.DataModels
+﻿namespace DataAccess.DataModels
 {
     public class ProductAvailability
     {
--- DataAccess/DatabaseInitialiser.cs
+++ DataAccess/DatabaseInitialiser.cs
@@ -8,6 +8,7 @@ namespace DataAccess
     {
         public static void Initialise(Context context)
         {
+            context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
 
             if (context.DiscountTypes.Any()) { return; }
@@ -15,10 +16,10 @@ namespace DataAccess
             /*--------------- ADD DISCOUNT TYPES ---------------*/
             var discountTypes = new DiscountType[]
             {
-                new DiscountType { ID = 1, Description = "£10 off your purchase", IsPercentage = true, DiscountValue = 10, Threshold = null },
-                new DiscountType { ID = 2, Description = "10% off your purchase", IsPercentage = false, DiscountValue = 10, Threshold = null },
-                new DiscountType { ID = 3, Description = "£10 off orders above £50", IsPercentage= true, DiscountValue = 10, Threshold = 50 },
-                new DiscountType { ID = 4, Description = "20% off your purchase", IsPercentage = false, DiscountValue = 20, Threshold = null }
+                new DiscountType { Description = "£10 off your purchase", IsPercentage = true, DiscountValue = 10, Threshold = null },
+                new DiscountType { Description = "10% off your purchase", IsPercentage = false, DiscountValue = 10, Threshold = null },
+                new DiscountType { Description = "£10 off orders above £50", IsPercentage= true, DiscountValue = 10, Threshold = 50 },
+                new DiscountType { Description = "20% off your purchase", IsPercentage = false, DiscountValue = 20, Threshold = null }
             };
 
             foreach (DiscountType dt in discountTypes)
@@ -36,9 +37,65 @@ namespace DataAccess
                 new Voucher { ID = "£10OFF", DiscountTypeID = 1, DiscountAvailableUntil = new DateTime(2015, 5, 30), OneTimeUse = false }
             };
 
+            foreach(Voucher v in vouchers)
+            {
+                context.Vouchers.Add(v);
+            }
+            context.Commit();
+
+            /*--------------- ADD SIZES ---------------*/
+            var sizes = new Size[]
+            {
+                new Size { SizeNumber = 4 },
+                new Size { SizeNumber = 4.5 },
+                new Size { SizeNumber = 5 },
+                new Size { SizeNumber = 6 }
+            };
+
+            foreach(Size s in sizes)
+            {
+                context.Sizes.Add(s);
+            }
             context.Commit();
 
             /*--------------- ADD PRODUCTS ---------------*/
+            var products = new Product[]
+            {
+                new Product
+                {
+                    ShoeName = "Leather Boots",
+                    Price = 34.99,
+                    PhotoFilepath = "leather-boots-01.jpg",
+                    Description = "With their versatile black colourway and practical block heels, " +
+                                  "the Castarika ankle boots from Christian Louboutin will have you striding " +
+                                  "into the new season with confidence. Made from leather, they have the " +
+                                  "label's signature lipstick-red soles – a recognizable finish for those in the know."
+                },
+                new Product
+                {
+                    ShoeName = "Colourful Sneakers",
+                    Price = 74.99,
+                    PhotoFilepath = "colourful-sneakers-01.jpg",
+                    Description = "Sneaker lovers will recognize Nike's iconic Air Max 95 design, refreshed with color blocked stripes, " +
+                                  "including leopard and floral prints. The tonal Air® sole, along with an embroidered swoosh, lend this look " +
+                                  "distinguished style – perfect for pairing with dressy outfits for an urban finish."
+                },
+                new Product
+                {
+                    ShoeName = "Floral Heels",
+                    Price = 55.00,
+                    PhotoFilepath = "floral-heels-01.jpg",
+                    Description = "These vibrant Keira sandals from Dolce & Gabbana reflect the house's jungle theme for SS20. Printed with brightly-colored" +
+                                  " birds of paradise and lush foliage, they're made with silk twill straps that reference '50s-era pin-up styles. They're " +
+                                  "perched atop 105mm heels wrapped in wicker."
+                }
+            };
+
+            foreach(Product p in products)
+            {
+                context.Products.Add(p);
+            }
+            context.Commit();
         }
     }
 }

41776caaa837c988f3fc7338eac4c4ddbf4a68bc
Yna Ramos <yna.francesca.ramos@gmail.com>
2020-12-29 20:35:04 +0000

    data from db successfully extracted, images showing on front page! :-)

---
+++ GoldenShoe/ViewModelBuilders/HomeViewModelBuilder.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess;
+using DataAccess.DataModels;
+using GoldenShoe.ViewModels;
+using System.Collections.Generic;
+
+namespace GoldenShoe.ViewModelBuilders
+{
+    public class HomeViewModelBuilder
+    {
+        private readonly Context _context;
+
+        public HomeViewModelBuilder(Context context)
+        {
+            _context = context;
+        }
+
+        public HomeViewModel GetAllProductsForDisplay()
+        {
+            var repo = new DataRepository(_context);
+
+            IList<Product> products = repo.GetAllProducts().Result;
+
+            var model = new HomeViewModel
+            {
+                Products = products,
+                InCart = new List<ShoppingCartItemViewModel>()
+            };
+
+            return model;
+        }
+    }
+}
---
+++ GoldenShoe/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.DataModels;
+using System.Collections.Generic;
+
+namespace GoldenShoe.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IList<Product> Products { get; set; }
+
+        public IList<ShoppingCartItemViewModel> InCart { get; set; }
+    }
+}
